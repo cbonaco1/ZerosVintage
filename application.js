b91501aa3e6ea64bb5e63db661c9c4fb8116f716
cbonaco1 <christian.bonacore@gmail.com>
2017-08-11 06:20:44 -0400

    Setup express-handlebars rendering views and layouts

--- application.js
+++ application.js
@@ -1,21 +1,28 @@
 var express = require('express'),
     app = express(),
     path = require('path'),
-    PORT = process.env.PORT || 9999;
+    handlebars = require('express-handlebars'),
+    PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.engine('hbs', handlebars({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+
 app.get("/", function(req, res) {
-    res.sendFile("/index.html");
+    res.render('index');
 });
 
-// app.get("/browse", function(req, res) {
-//     res.sendFile("/browse.html");
-// });
-//
-// app.get("/pdp", function(req, res) {
-//     res.sendFile("/pdp.html");
-// });
+app.get("/browse", function(req, res) {
+    res.render("browse");
+});
+
+app.get("/pdp", function(req, res) {
+    res.render("pdp");
+});
 
 app.listen(PORT, function() {
     console.log('App is listening on port ' + PORT);
