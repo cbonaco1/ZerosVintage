37b464fa5df162699278c03b27b917743ac13b0f
cbonaco1 <christian.bonacore@gmail.com>
2017-08-26 09:27:22 -0400

    Hamburger menu content slide out from right

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
     // On click of hamburger menu, transition menu
-    $("#more-hamburger").on('click', function() {
+    $("#more-hamburger").on('click', function(event) {
+        event.preventDefault();
         $("#overlay-wrapper").show();
         $("#hamburger-menu-content").addClass("slide-in");
     });
 
-    $("#close-hamburger").on('click', function() {
+    $("#close-hamburger").on('click', function(event) {
+        event.preventDefault();
         $("#overlay-wrapper").hide();
         $("#hamburger-menu-content").removeClass("slide-in");
     });
