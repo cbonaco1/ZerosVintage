cd3ace2aac977329566ad87a6eb7bafd03863c36
cbonaco1 <christian.bonacore@gmail.com>
2017-08-30 06:55:11 -0400

    Basic Home top nav bar

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
+    $("#desktop-more-menu-item").on('click', function(event) {
+        var _this = $(this);
+        event.preventDefault();
+        if (_this.hasClass('expanded')) {
+            _this.removeClass('expanded');
+            $("#desktop-more-submenu").slideUp();
+        } else {
+            _this.addClass('expanded');
+            $("#desktop-more-submenu").slideDown();
+        }
+    });
+
+    $("#desktop-more-menu-item").on('mouseenter focusin', function(event) {
+        $("#desktop-more-submenu").slideDown();
+    });
+
+    $("#desktop-more-submenu").on('mouseleave', function(event) {
+        $("#desktop-more-submenu").slideUp();
+    });
+    // Not sure if i need this
+    // $("#desktop-more-menu-item").on('focusout', function(event) {
+        // $("#desktop-more-submenu").slideUp();
+    // });
+
     // On click of hamburger menu, transition menu
     $("#more-hamburger").on('click', function(event) {
         event.preventDefault();
